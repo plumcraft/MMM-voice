74eff36ffeb83abbdd3f3ccd586ca466bd0b0b73
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 21:43:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-{ module: 'mmm-voice', position: 'bottom_bar', config: { keyword: 'ENV_VOICE_CODE' , microphone: 1, debug: true }, classes: 'plumcraft' }
+{ module: 'mmm-voice', position: 'bottom_bar', config: { keyword: 'ENV_VOICE_CODE' , microphone: 1, debug: true }, classes: 'plumcraft' },
